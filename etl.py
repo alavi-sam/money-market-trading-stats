61c845d84e0b23f4d31616732f94637b2ac4ec55
Sam Alavi <sam.alavi@hotmail.com>
2026-02-10 02:49:15 -0500

    Add function to clean money market repo trading data sheets

--- etl.py
+++ etl.py
@@ -214,7 +214,26 @@ def clean_bonds_repo_sheet(path):
 
 
 
+def clean_mmkt_repo_sheet(path):
+    df = pd.read_excel(path, sheet_name='MMKT_REPO', skiprows=5, skipfooter=5)
+    df.columns = [col.split(' / ')[0].strip() for col in df.columns]
+    df.drop([df.columns[1]], axis=1, inplace=True)
+    df = df[~df['The Month'].isna()]
+    df = df[~df['The Month'].str.startswith('Q') & ~df['The Month'].str.startswith('TOTAL')]
+    df.fillna(0, inplace=True)
+    df.reset_index(drop=True, inplace=True)
+    df['month'] = df['The Month'].str.split('/').map(lambda x: month_dict[x[0].strip()])
+    df['month'] = df['month'].astype('str')
+    df['year'] = df['The Month'].str[-4:]
+    df['date'] = pd.to_datetime(df['month'] + '-' + df['year']).dt.date
+    df.head()
+    df.drop(columns=['The Month', 'TOTAL'], inplace=True)
+    df_melted = df.melt(id_vars=['date', 'month', 'year'], 
+                        var_name='instruments', 
+                        value_name='volume')
+    return df_melted
+
 
 path = 'raw_data/2024-Bond-and-Money-Market-Secondary-Trading-Statistics.xlsx'
 
-print(clean_bonds_repo_sheet(path))
+print(clean_mmkt_repo_sheet(path))
